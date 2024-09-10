e2018ac28cfd26d3a13eeeed5c5ee908ef70d9a6
Muhammad Faarid Khalid <khalidhabibe19@gmail.com>
2024-09-10 15:31:23 +0500

    add files and folders

---
+++ app.js
@@ -0,0 +1,293 @@
+
+// chapter 5
+// Q1
+// var num1=3;
+// var num2=5;
+// var sum=num1+num2;
+
+// document.write("sum of" + " "  + num1+ " " + num2 + " " + " "  +"is"+ " " + sum)
+// Q2
+
+// var num1=3;
+// var num2=5;
+// var sum=num1-num2;
+// document.write("subraction of" + " "  + num1+ " " + num2 + " " + " "  +"is"+ " " + sum +"<br>")
+
+// var num1=3;
+// var num2=5;
+// var sum=num1*num2;
+// document.write("product of" + " "  + num1+ " " + num2 + " " + " "  +"is"+ " " + sum +"<br>")
+// var num1=3;
+// var num2=5;
+// var sum=num1/num2;
+// document.write("division of" + " "  + num1+ " " + num2 + " " + " "  +"is"+ " " + sum +"<br>")
+// var num1=3;
+// var num2=5;
+// var sum=num1%num2;
+// document.write("modulus of" + " "  + num1+ " " + num2 + " " + " "  +"is"+ " " + sum+"<br>")
+// Q3
+// var num;
+// document.write("value after variable declaration is ??" + "<br>" )
+// num=5;
+// document.write("Initial value: 5 <br>")
+// num++;
+// document.write("value after increment is: 6 <br>")
+// num + 7;
+// document.write("value after addition is :13 <br>")
+// num--;
+// document.write("value after decrement is :12 <br>")
+// var remainder=num%3;
+// document.write("the remainder is 0")
+// Q4
+// var ticketPrice=600;
+// var total=600*5
+// document.write("Total cost to buy 5 ticket is"+ total)
+// Q5
+// var number=4;
+// document.write("4x1=" + number+  "   <br>") 
+// document.write("4x2=" + number*2 + " <br> ")
+// document.write("4x3=" + number*3 + " <br> ")
+// document.write("4x4=" + number*4 + " <br> ")
+// document.write("4x5=" + number*5 + " <br> ")
+// document.write("4x6=" + number*6 + " <br> ")
+// document.write("4x7=" + number*7 + " <br> ")
+// document.write("4x8=" + number*8 + " <br> ")
+// document.write("4x9=" + number*9 + " <br> ")
+// document.write("4x10="+number*10)
+// Q6
+// var celcius =25;
+// var fahrenheit = (celcius * 9 / 5) + 32;
+// document.write(celcius + "°C is " + fahrenheit + "°F <br>");
+// var fahrenheitInput = 70;
+// var celcius_converted=(fahrenheitInput - 32) * 5 / 9;
+// document.write(fahrenheitInput + "°F is " + celcius_converted + "°C");
+// Q7
+// var item1=650
+// var item2=100
+// var quantityofitem1=3
+// var quantityofitem2= 7
+// var shippingCharges=100
+// var totalcost=(item1*3 + item2*7 + 100)
+// document.write("<h1>Shopping Cart</h1>")
+// document.write("Price of item 1 is: " + item1 + "<br>");
+// document.write("Price of item 2 is: " + item2 + "<br>");
+// document.write("Ordered quantity of item 1 is: " + quantityofitem1+ "<br>");
+// document.write("Ordered quantity of item 2 is: " + quantityofitem2+ "<br>");
+// document.write("Shipping charges: " + shippingCharges + "<br> <br>");
+// document.write("Total cost of your order is: " + totalcost);
+// Q8
+// var totalmarks=980;
+// var markobtained=804;
+// var percentage=markobtained/totalmarks*100;
+// document.write("<h1>Marks Sheet</h1> <br>")
+// document.write("totalmarks"+":"+ totalmarks+"<br>")
+// document.write("marksobtained"+":"+ markobtained+"<br>")
+// document.write("percentage"+":"+percentage+"<br>")
+// Q9
+// var usdollar=104.80
+// var saudiriyal=28
+// var total=104.80*10 + 28*25
+// document.write("<h1>Currency in PKR</h1>")
+// document.write("Total Currency in PKR :"+ total)
+// Q10
+// var num=20
+// var total=(num+5)*10/2
+// document.write("<h1>Result</h1> <br>")
+// document.write("The result is "+ total)
+// Q11
+// var currentyear=2016
+// var birthyear=1992
+// var age=currentyear-birthyear
+// document.write("<h1>Age</h1> <br>")
+// document.write("Current year:"+ currentyear+"<br>")
+// document.write("Current year:"+ birthyear+"<br>")
+// document.write("Current year:"+ age)
+// Q12
+// var radius = 20; 
+// var pi = 3.142;
+// var circumference = 2 * pi * radius;
+// var area = pi * radius * radius;
+// document.write("<h1>The Geometrizer</h1> <br>")
+// document.write("The radius of a circle: " + radius + "<br>")
+// document.write("The circumference is " + circumference+"<br>");
+// document.write("The area is " + area);
+// Q13
+// var favoriteSnack = "ChocolateChip";
+// var currentAge = 15;
+// var maximumAge = 65;
+// var amountPerDay = 3;
+// var totalSnacks = (maximumAge - currentAge) * 365 * amountPerDay;
+// document.write("<h1>The Lifetime Supply Calculator</h1> <br>")
+// document.write("Favourite Snack :" + favoriteSnack +"<br>");
+// document.write("Current Age " + currentAge+"<br>");
+// document.write("Estimated Maximum Age " + maximumAge+"<br>");
+// document.write("Amount of Snacks Per Day " + amountPerDay+"<br>");
+// document.write("You will need " + totalSnacks + " " + favoriteSnack + "s to last you until the ripe old age of " + maximumAge + ".");
+
+
+
+// chapter6-9
+// Q1
+// var a = 10;
+// console.log("The value of a is: " + a);
+// console.log("The value of ++a is: " + (++a));
+// console.log("Now the value of a is: " + a);
+// console.log("The value of a++ is: " + (a++));
+// console.log("Now the value of a is: " + a);
+// console.log("The value of --a is: " + (--a));
+// console.log("Now the value of a is: " + a);
+// console.log("The value of a-- is: " + (a--));
+// console.log("Now the value of a is: " + a);
+
+// Q2
+// var a = 2
+// var b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write("a is"+ a +"<br>")
+// document.write("b is"+ b+"<br>")
+// document.write("result is"+ result)
+// --a=1 - --b=0 + ++b=1 + b--=1  1-0+1+1=3
+// Q3
+// var user=prompt("enter your name")
+// Q4
+// var number = prompt("Enter a number for its multiplication table (default is 5):");
+// number = parseInt(number, 10);
+// if (isNaN(number)) {    number = 5; 
+// }
+// for (var i = 1; i <= 10; i++) {
+//     console.log(number + " x " + i + " = " + (number * i));
+// }
+// Q5
+// var English="english";
+// var Math="math";  
+// var Urdu="urdu"; 
+// var totalMarks = 100;
+// var obtainedMarks1 = 54;
+// var obtainedMarks2 =54 ;
+// var obtainedMarks3 = 48;
+// var totalObtainedMarks = obtainedMarks1 + obtainedMarks2 + obtainedMarks3;
+// var totalMaxMarks = totalMarks * 3;
+// var percentage = (totalObtainedMarks / totalMaxMarks) * 100;
+// document.write("<h1>Marks Summary</h1>");
+// document.write("<table cellpadding='10'>");
+// document.write("<tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th><th>Percentage</th></tr>");
+// document.write("<tr><td>" + English + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks1 + "</td><td>" + (obtainedMarks1 / totalMarks * 100).toFixed(2) + "%</td></tr>");
+// document.write("<tr><td>" + Math + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks2 + "</td><td>" + (obtainedMarks2 / totalMarks * 100).toFixed(2) + "%</td></tr>");
+// document.write("<tr><td>" + Urdu + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks3 + "</td><td>" + (obtainedMarks3 / totalMarks * 100).toFixed(2) + "%</td></tr>");
+// document.write("<tr><th>Total</th><th>" + totalMaxMarks + "</th><th>" + totalObtainedMarks + "</th><th>" + percentage.toFixed(2) + "%</th></tr>");
+// document.write("</table>");
+
+
+
+// chapter6 Math experssion II
+// Q1
+// var x=x++;
+// var x = x +1;
+// Q2
+// var x=100
+// x--;
+// Q3
+// var x=50;
+// var y=x++
+// x will be 51
+// y will be 50
+// Q4
+// var y=50;
+// var z=--y
+// y will be 50
+// z will be 49
+// Q5
+// var num=2
+// var newNum = num--; 
+// Q6
+// var variable=4
+// var originalValue = variable++;
+// Q7
+// var num = 5; 
+// num++;    
+// alert(num);  
+
+// chapter7 mathexpression III
+
+
+// Q1
+// var calculatedNum = 2 + (2 * 6);
+// console.log(calculatedNum); 
+// the calculated value is 14
+// Q2
+// var calculatedNum = (2+2)*6;
+// console.log(calculatedNum); 
+// the calculated value is 24
+// Q3
+// var calculatedNum = (2 + 2) * (4 + 2);
+// console.log(calculatedNum);
+// the calculated value is 32
+// Q4
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// console.log(calculatedNum);
+// the calculated value is 18
+// Q5
+// var statement=(2 + (2 * (4 + 10)));
+// console.log(statement)
+// it producing 56
+// Q6
+// var statement=(2+(2*4)+10);
+// console.log(statement)
+// it producing 20
+// Q7
+// var statement= 4 / (2 * 4);
+// console.log(statement)
+// it producing 0.5
+
+// chapter8Concatenating Text
+//     Strings
+
+// Q1
+// var num="2"+"2""!"
+// console.log(num)
+// Q2
+// var message=("Hello" + "Dolly")"!";
+// alert(message)
+// Q3
+// var message="33"+3
+// alert(message)
+// Q4
+// alert("Pakistan" + "Zindabad");
+// Q5
+// var result="the num is "+45
+// console.log(result)
+// Q6
+// var firststring="Hello"
+// var secondstring="World"
+// var concadinatingstring="Hello"+"World!"
+// console.log(concadinatingstring)
+
+
+// chapter9 Prompts
+
+// Q1
+// var firstName = prompt("Enter first name");
+// Q2
+// var country=prompt("country","china")
+// Q3
+// var yourName = prompt("Enter Your Name");
+// Q4
+// var prompt=prompt("name","zaid")
+// Q5
+// var message = "Enter your favorite color";
+// var defaultResponse = "Blue";
+
+// var userResponse = prompt(message, defaultResponse + user);
+
+// var user="red";
+
+// Q6
+
+// var userResponse = prompt("What is your favorite movie?", "Inception");
+// alert("You entered: " + userResponse);
+
+
+
+
+
+
